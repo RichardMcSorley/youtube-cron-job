1b581455bf5edfeb9ecbcea06f91ee48046241d7
Richard McSorley <yourevilmonk@gmail.com>
2019-04-27 17:36:57 -0500

    mq error handling

--- api/mq/index.js
+++ api/mq/index.js
@@ -4,20 +4,27 @@ const startClient = ()=>{
     return new Promise((resolve, reject) => {
         const service = `amqp://${process.env.MQLIGHT_SERVICE_HOST}:${process.env.MQLIGHT_SERVICE_PORT_AMQP}`;
         console.log(`attempting to connect to mq service `)
-        const sendClient = mqlight.createClient({service});
-        sendClient.on('started', function() {
+        const client = mqlight.createClient({service}, (err)=>{
+            console.log(err);
+            reject(err)
+        });
+        client.on('started', function() {
             console.log('started mq');
             resolve(sendClient)
         });
+        client.on('error', function (err) {
+            console.error(err)
+            process.exit(1);
+          })
     });
 }
 const sendMessage = ({client, topic, data}) =>{
     return new Promise((resolve, reject)=>{
-        client.send(topic, data, function (err, data) {
+        client.send(topic, data, function (err) {
             if(err){
                 reject(err);
             }
-            console.log('Sent mq:', data, data);
+            console.log('Sent mq:', topic, data);
             resolve();
         });
     });
